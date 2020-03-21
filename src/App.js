53acc70b52bc01edb59ea8033fc7343526edfacc
neldo <neldin.dzeko@gmail.com>
2020-03-21 16:25:55 +0100

    add book component.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false
-  }
+  };
 
   render() {
     return (
@@ -47,4 +47,4 @@ class BooksApp extends React.Component {
   }
 }
 
-export default BooksApp
+export default BooksApp;
